74ea2cbfd78d5c6f3743689998310e58a31a1090
Ricardo Godoy <rq.godoy@gmail.com>
2021-12-05 19:33:46 -0300

    Initial commit

---
+++ Runtime/HierarchySparator.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class HierarchySparator : MonoBehaviour
+{
+  [ColorUsage(showAlpha: false)]
+  private Color m_BarColor;
+  public Color BarColor {
+    get {
+      if(m_BarColor == null) return Color.black;
+      return m_BarColor;
+    }
+    set {
+      value.a = 1f;
+      m_BarColor = value;
+    }
+  }
+
+  static HierarchySparator()
+  {
+    EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+  }
+
+  [MenuItem("GameObject/Separator")]
+  public static void CreateSeparator(MenuCommand menuCommand)
+  {
+    GameObject separator = new GameObject("Separator");
+    separator.AddComponent<HierarchySparator>();
+    GameObjectUtility.SetParentAndAlign(separator, menuCommand.context as GameObject);
+    Undo.RegisterCreatedObjectUndo(separator, "Create " + separator.name);
+    Selection.activeObject = separator;
+  }
+
+  static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+  {
+    GameObject gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+    if (gameObject == null) return;
+    if (!gameObject.TryGetComponent(out HierarchySparator hierarchy)) return;
+
+    Color color = hierarchy.BarColor;
+
+    EditorGUI.DrawRect(selectionRect, color);
+    EditorGUI.DropShadowLabel(selectionRect, $"{gameObject.name.ToUpperInvariant()}");
+  }
+}
