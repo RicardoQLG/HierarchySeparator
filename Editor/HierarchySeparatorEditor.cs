29899f439c2e52666297b2ecf569246311543234
Gabriel Bigardi <42253181+GabrielBigardi@users.noreply.github.com>
2022-02-19 14:58:15 -0300

    Added outlines, converted to SerializedProperty, multiediting, others...

--- Editor/HierarchySeparatorEditor.cs
+++ Editor/HierarchySeparatorEditor.cs
@@ -2,19 +2,40 @@ using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(HierarchySeparator))]
+[CanEditMultipleObjects]
 public class HierarchySeparatorEditor : Editor
 {
-  public HierarchySeparator variable;
+    //public HierarchySeparator variable;
 
-  public void OnEnable()
-  {
-    variable = target as HierarchySeparator;
-  }
+    private SerializedProperty _outlineSize;
+    private SerializedProperty _outlineColor;
+    private SerializedProperty _barColor;
+    private SerializedProperty _textColor;
 
-  public override void OnInspectorGUI()
-  {
-    base.OnInspectorGUI();
-    variable.BarColor = EditorGUILayout.ColorField(new GUIContent("Bar Color"), variable.BarColor, true, false, false);
-	variable.TextColor = EditorGUILayout.ColorField(new GUIContent("Text Color"), variable.TextColor, true, false, false);
-  }
+    public void OnEnable()
+    {
+        //variable = target as HierarchySeparator;
+        _outlineSize = serializedObject.FindProperty("m_OutlineSize");
+        _outlineColor = serializedObject.FindProperty("m_OutlineColor");
+        _barColor = serializedObject.FindProperty("m_BarColor");
+        _textColor = serializedObject.FindProperty("m_TextColor");
+    }
+    
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        //variable.OutlineSize = EditorGUILayout.IntField("Outline Size", variable.OutlineSize);
+        //variable.OutlineColor = EditorGUILayout.ColorField(new GUIContent("Outline Color"), variable.OutlineColor, true, false, false);
+        //variable.BarColor = EditorGUILayout.ColorField(new GUIContent("Bar Color"), variable.BarColor, true, false, false);
+        //variable.TextColor = EditorGUILayout.ColorField(new GUIContent("Text Color"), variable.TextColor, true, false, false);
+
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(_outlineSize);
+        EditorGUILayout.PropertyField(_outlineColor);
+        EditorGUILayout.PropertyField(_barColor);
+        EditorGUILayout.PropertyField(_textColor);
+
+        serializedObject.ApplyModifiedProperties();
+    }
 }
